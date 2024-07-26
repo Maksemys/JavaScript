367e59ea160ae2f88639afef00dbf2c71c50dd80
Maksemys <158852742+Maksemys@users.noreply.github.com>
2024-07-26 18:46:24 +0300

    Add files via upload

--- lesson_03/js/script3.js
+++ lesson_03/js/script3.js
@@ -4,21 +4,20 @@ alert('Вгадайте число від 1 до 5 за 2 спроби')
 const minNumber = 1
 const maxNumber = 5
 const randomNumber = minNumber + Math.floor(Math.random()*(maxNumber - minNumber + 1))
+let userChoice = parseFloat(prompt('Перша спроба! Введіть число від 1 до 5', '0'))
 
-const userChoiceFirst = parseFloat(prompt('Перша спроба! Введіть число від 1 до 5', '0'))
-const userChoiceSecond = parseFloat(prompt('Друга спроба!Введіть число від 1 до 5', '0'))
+// Обчислюємо
 
+if (userChoice === randomNumber) {
+	alert(`Вітаємо! Ви вгадали число з першої спроби! Це число: ${randomNumber}`)
+} else {
+	alert("Ви ввели не правильно! Спробуйте ще раз!")
 
-// Обчислення
-	if (userChoiceFirst === randomNumber || userChoiceSecond === randomNumber)
-		alert(`Вітаєм! Ви вгадали ${randomNumber}!`)
-	else alert('Ви не вгадали!!!!!!!!')
-
-
-	document.write (`
-		<div>Перший вибір = ${userChoiceFirst}</div>
-		<div>Другий вибір = ${userChoiceSecond}</div>
-		<div>Випадкове число = ${randomNumber}</div>
-		`)
-
+	userChoice = parseFloat(prompt('Друга спроба! Введіть число від 1 до 5', '0'))
+	if (userChoice === randomNumber) {
+		alert(`Вітаємо! Ви вгадали число з другої спроби! Це число: ${randomNumber}`)
+	} else {
+		alert(`У вас більше не залишилось спроб. Це число ${randomNumber}`)
+	}
+}
 
