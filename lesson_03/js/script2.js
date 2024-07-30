af3a602ced3c7a9e67d9d99c1ef57424eba005d0
Maksemys <158852742+Maksemys@users.noreply.github.com>
2024-07-30 12:41:51 +0300

    Add files via upload

--- lesson_03/js/script2.js
+++ lesson_03/js/script2.js
@@ -1,8 +1,15 @@
 // Вводимо необхідні данні
 let productPrice = parseFloat(prompt('Введіть ціну товару у грн.', '0'))
 let moneyQuantity = parseFloat(prompt('Введіть кількість грошей у грн.', '0'))
+const balanceMoney = moneyQuantity - productPrice
+const lotteryTicket = 4
 
 // Обчислення
 if(productPrice > moneyQuantity)
 	alert("Вибачте, але у вас не достатьньо коштів! У покупці відмовлено!")
-else alert("Пропонуємо купити лотерею за 4 грн")
+else {
+	if (balanceMoney >= lotteryTicket)
+		alert("Пропонуємо купити лотерею за 4 грн")
+	else 
+		alert ('Дякуємо за покупку!')
+} 
